bc5c8b0e9e61613980a9cfa26e692b1f3ad219f9
Miguel Lupani <mlupani2@gmail.com>
2024-11-21 10:13:55 -0300

    ultimos cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import auth from 'auth-astro';
 
 import netlify from '@astrojs/netlify';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), auth()],
+  integrations: [db(), auth(), icon()],
   output: 'server',
   adapter: netlify(),
 });
